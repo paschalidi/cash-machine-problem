d0f42e0e01563965f9994a3a1894380f9ef324a5
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2018-06-09 15:40:49 +0200

    release(master): writes tests and implements calculator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 'use strict';
 
-export const calculateNotes = (value) => true;
+export const calculateNotes = (value) => {
+  const notes = [100, 50, 20, 10];
+  const minNote = notes[notes.length - 1];
+  if (value % minNote === 0)
+    return notes.map(index => {
+        let originalValue = value;
+        value = value - Math.floor(value / index) * index;
+        return Math.floor(originalValue / index);
+      }
+    );
+  if (value === null) return [];
+
+  return 'throw InvalidArgumentException';
+};
+
+export default calculateNotes;
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,57 @@
 const expect = require('chai').expect;
+const { calculateNotes } = require('./index.js');
 
-
-describe('the implementation of a cash mashine', () => it('should work', () => {
-    expect(true).to.be.true;
-  })
+describe('calculate(null)',
+  () => it('should return: []',
+    () => {
+      expect(calculateNotes(null)).to.be.an('array');
+    })
+);
+describe('calculateNotes(5)',
+  () => it('should return: throw InvalidArgumentException',
+    () => {
+      expect(calculateNotes(5)).to.be.an('string');
+    })
+);
+describe('calculate(0)',
+  () => it('should return: [0,0,0,0]',
+    () => {
+      expect(calculateNotes(0)).to.have.ordered.members([0, 0, 0, 0]);
+    })
+);
+describe('calculate(100)',
+  () => it('should return [1,0,0,0]',
+    () => {
+      expect(calculateNotes(100)).to.have.ordered.members([1, 0, 0, 0]);
+    })
+);
+describe('calculate(550)',
+  () => it('should return [5,1,0,0]',
+    () => {
+      expect(calculateNotes(550)).to.have.ordered.members([5, 1, 0, 0]);
+    })
+);
+describe('calculate(1550)',
+  () => it('should return [15,1,0,0]',
+    () => {
+      expect(calculateNotes(550)).to.have.ordered.members([5, 1, 0, 0]);
+    })
+);
+describe('calculate(1570)',
+  () => it('should return [15,1,1,0]',
+    () => {
+      expect(calculateNotes(570)).to.have.ordered.members([5, 1, 1, 0]);
+    })
+);
+describe('calculate(580)',
+  () => it('should return [5,1,1,1]',
+    () => {
+      expect(calculateNotes(580)).to.have.ordered.members([5, 1, 1, 1]);
+    })
+);
+describe('calculate(580)',
+  () => it('should return [5,1,2,0]',
+    () => {
+      expect(calculateNotes(590)).to.have.ordered.members([5, 1, 2, 0]);
+    })
 );
