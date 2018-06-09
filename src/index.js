9e9ab3e52a3ddeaa900f2e9a65d8f5948f804a62
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2018-06-09 15:52:51 +0200

    feat(master): cleans

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 export const calculateNotes = (value) => {
   const notes = [100, 50, 20, 10];
   const minNote = notes[notes.length - 1];
+
   if (value % minNote === 0)
     return notes.map(index => {
         let originalValue = value;
@@ -10,9 +11,9 @@ export const calculateNotes = (value) => {
         return Math.floor(originalValue / index);
       }
     );
-  if (value === null) return [];
+  if (value === null)
+    return [];
 
   return 'throw InvalidArgumentException';
 };
-
 export default calculateNotes;
